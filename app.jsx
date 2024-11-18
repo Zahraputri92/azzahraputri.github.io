bb2adfca04bc75c66babd7119ecbe0d9ebb31ad2
Zahraputri92 <zcahyani92@gmail.com>
2024-11-18 17:54:05 +0700

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">React Hook Form Example</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
